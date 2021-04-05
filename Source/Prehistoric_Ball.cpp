7d4238cfc73efc5ba9163bbf19427d0fbd29f4ae
Kane Thompson-Hall <57604470+KaneTHall@users.noreply.github.com>
2021-04-05 21:06:25 +0100

    Delete Prehistoric_Ball.cpp

--- Source/Prehistoric_Ball.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Prehistoric_Ball.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prehistoric_Ball, "Prehistoric_Ball" );
